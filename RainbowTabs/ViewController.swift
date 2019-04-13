b0bcb4c7ef183ef429ba8edef6f68fb78e74555b
Sim Yong Seng <simyseng@gmail.com>
2019-04-13 07:10:46 +0700

    Initialized commit

---
+++ RainbowTabs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Sim Yong Seng on 13/4/19.
+//  Copyright © 2019 Sim Yong Seng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
